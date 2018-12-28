8a5d246379a39741953a4262a37a9f9c74de2bf5
pumpkinzomb <pumpkinzomb@gmail.com>
2018-12-29 00:37:26 +0900

    Updates

---
+++ precache-manifest.529ab577f1583c60c59079b861a6e3f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "7138841e3957c1158f88",
+    "url": "/movie_app/static/js/main.7138841e.chunk.js"
+  },
+  {
+    "revision": "711695575596b6ad21c5",
+    "url": "/movie_app/static/js/1.71169557.chunk.js"
+  },
+  {
+    "revision": "7138841e3957c1158f88",
+    "url": "/movie_app/static/css/main.73fc7b20.chunk.css"
+  },
+  {
+    "revision": "079057e6d7d09470637c41da4d702cc4",
+    "url": "/movie_app/index.html"
+  }
+];
